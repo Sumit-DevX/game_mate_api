a976b1d3b0ff30810c9ba7a8d4c76d2eb63b2e4e
Sumit Nayak <sumit.debugged@gmail.com>
2026-08-26 18:09:13 +0530

    feat:Separate file for pydantic models

---
+++ models.py
@@ -0,0 +1,58 @@
+from pydantic import BaseModel, ConfigDict
+
+class UserModel(BaseModel):
+
+    name : str
+    age: int
+    email: str
+    country: str
+
+class UserResponseModel(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    name: str
+    age: int
+    email: str
+    country: str
+
+
+class GameModel(BaseModel):
+    name: str
+
+class GameResponseModel(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id : int
+    name : str
+
+class LFGPostModel(BaseModel):
+    user_id : int
+    game_id : int 
+    title : str
+    players_needed : int
+    message: str | None
+
+class LFGPostResponseModel(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id : int
+    user_id : int 
+    game_id : int
+    title : str
+    players_needed: int
+    message: str | None
+
+
+
+class JoinRequestModel(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    user_id : int
+    lfg_post_id : int 
+    status: str
+
+
+class JoinRequestUpdateModel(BaseModel):
+    status : str
+
